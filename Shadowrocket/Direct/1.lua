c992b6239cb587d5a11d912780064549f083a4cb
zxcvbnm6688 <32958980+zxcvbnm6688@users.noreply.github.com>
2023-06-05 21:35:42 +0800

    Update 1.lua

--- Shadowrocket/Direct/1.lua
+++ Shadowrocket/Direct/1.lua
@@ -1,14 +1,14 @@
-mode=wap; 
+mode=wap;
 listen_port=65080;
 daemon=on;
 worker_proc=0;
 uid=0;
-http_ip=153.3.236.22;
+http_ip=cloudnproxy.n.shifen.com;
 http_port=443;
 http_del="Host,X-Online-Host";
 http_first="[M] http://[H][U] [V]\r\nHost: 153.3.236.22:443\r\nX-T5-Auth: 683556433\r\nUser-Agent:baiduboxapp\r\n";
 https_connect=on;
-https_ip=153.3.236.22;
+https_ip=cloudnproxy.n.shifen.com;
 https_port=443;
 https_del="User-Agent";
 https_first="[M] [H] [V]\r\nHost: 153.3.236.22:443\r\nX-T5-Auth: 683556433\r\nUser-Agent:baiduboxapp\r\n";
