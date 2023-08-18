db226cbd9b8ce0db9a506cb3ffa1a23ed3f3a0de
zxcvbnm6688 <32958980+zxcvbnm6688@users.noreply.github.com>
2023-08-19 00:18:31 +0800

    Update wps.js

--- QuantumultX/Rewrite/Scripts/wps.js
+++ QuantumultX/Rewrite/Scripts/wps.js
@@ -26,12 +26,12 @@ var obj = {
   exp: 0,
   level: 3,
   privilege: [
-    { spid: "data_recover", times: 0, expire_time: 1846256142 },
-    { spid: "ocr", times: 0, expire_time: 1846256142 },
-    { spid: "pdf2doc", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_merge", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_sign", times: 0, expire_time: 1846256142 },
-    { spid: "pdf_split", times: 0, expire_time: 1846256142 }
+    { spid: "data_recover", times: 0, expire_time: 4092599349 },
+    { spid: "ocr", times: 0, expire_time: 4092599349 },
+    { spid: "pdf2doc", times: 0, expire_time: 4092599349 },
+    { spid: "pdf_merge", times: 0, expire_time: 4092599349 },
+    { spid: "pdf_sign", times: 0, expire_time: 4092599349 },
+    { spid: "pdf_split", times: 0, expire_time: 4092599349 }
   ],
   result: "ok",
   total_buy: 0,
@@ -41,15 +41,15 @@ var obj = {
     name: "超级会员",
     has_ad: 0,
     memberid: 40,
-    expire_time: 1846256142,
+    expire_time: 4092599349,
     enabled: [
-      { memberid: 40, name: "超级会员", expire_time: 1846256142 },
-      { memberid: 20, name: "WPS会员", expire_time: 1846256142 },
-      { memberid: 12, name: "稻壳会员", expire_time: 1846256142 }
+      { memberid: 40, name: "超级会员", expire_time: 4092599349 },
+      { memberid: 20, name: "WPS会员", expire_time: 4092599349 },
+      { memberid: 12, name: "稻壳会员", expire_time: 4092599349 }
     ]
   },
   wealth: 0,
-  expire_time: 1846256142
+  expire_time: 4092599349
 };
 
 $done({ body: JSON.stringify(obj) });
