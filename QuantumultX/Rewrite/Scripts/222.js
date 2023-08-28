e7565dbcd929235eb5abe6c7e629d0afae2c7d4a
zxcvbnm6688 <32958980+zxcvbnm6688@users.noreply.github.com>
2023-08-29 01:04:32 +0800

    Update and rename 666.js to 222.js

--- QuantumultX/Rewrite/Scripts/222.js
+++ QuantumultX/Rewrite/Scripts/222.js
@@ -1,7 +1,14 @@
 /*************************************
+项目名称：WPS Office
+下载地址：https://t.cn/A6KOhd30
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
 
 [rewrite_local]
-^https?:\/\/account\.wps\.cn\/api\/users url script-response-body https://raw.githubusercontent.com/zxcvbnm6688/jiedian/main/QuantumultX/Rewrite/Scripts/666.js
+^https?:\/\/account\.wps\.cn\/api\/users url script-response-body https://raw.githubusercontent.com/zxcvbnm6688/jiedian/main/QuantumultX/Rewrite/Scripts/222.js
 
 [mitm]
 hostname = *.wps.cn
